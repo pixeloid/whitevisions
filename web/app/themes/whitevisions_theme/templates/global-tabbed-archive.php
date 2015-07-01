9100450be4eb26cf5bb1d239366e83a83e9fa43b
Pixeloid <olah.gergely@pixeloid.hu>
2015-07-01 13:48:15 +0200

    disabled auto active tab

--- web/app/themes/whitevisions_theme/templates/global-tabbed-archive.php
+++ web/app/themes/whitevisions_theme/templates/global-tabbed-archive.php
@@ -11,7 +11,7 @@
             </h2>
             <ul class="nav nav-list hidden-xs" role="tablist">
               <?php $first = true; while (have_posts()) : the_post();  ?>
-              <li <?php if($first): ?>class="active" <?php endif; ?>role="presentation">
+              <li <?php if($first): ?>class="" <?php endif; ?>role="presentation">
                 <a aria-controls="<?php echo $post->post_name; ?>" data-toggle="tab" href="#<?php echo $post->post_name; ?>" role="tab" aria-expanded="false"><?php the_title(); ?></a>
               </li>
               <?php if($first) $first = false; ?>
@@ -24,7 +24,7 @@
         <div class="col-md-8 col-md-push-1 tab-content">
           <?php $first = true;  while (have_posts()) : the_post(); ?>
 
-            <div class="text-left tab-pane <?php if($first): ?> active<?php endif; ?>" id="<?php echo $post->post_name; ?>">
+            <div class="text-left tab-pane <?php if($first): ?> <?php endif; ?>" id="<?php echo $post->post_name; ?>">
             <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
             </div>
             <?php if($first) $first = false; ?>
