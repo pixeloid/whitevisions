89cb1b3f2cdac93f6e7cd485bb163c61ff2f50ce
Pixeloid Office <olah.gergely@pixeloid.hu>
2015-11-24 09:50:47 +0100

    no message

--- web/app/themes/whitevisions_theme/templates/page-gallery-links.php
+++ web/app/themes/whitevisions_theme/templates/page-gallery-links.php
@@ -11,7 +11,7 @@
     </h1>
     <div class='row'>
     
-    <?php if($num == 2): ?>
+    <?php if($num == 2 || $num == 4): ?>
 
         <?php $first = true; while (have_posts()) : the_post();  ?>
         <div class='col-sm-6'>
@@ -32,10 +32,7 @@
         <?php if($first) $first = false; ?>
         <?php endwhile; ?>
     
-    <?php endif; ?>
-
-
-    <?php if($num == 3): ?>
+    <?php elseif($num == 3): ?>
 
         <?php $first = true; while (have_posts()) : the_post();  ?>
           
@@ -62,6 +59,62 @@
           <?php else: ?>
 
             <div class='col-sm-12'>
+              <div class='gallery-inner'>
+                <a href=' <?php the_permalink(); ?>'>
+                  <?php
+              if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                the_post_thumbnail('large', array( 'class' => 'img-responsive' ));
+              } 
+              ?>
+                  <div class='text'>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php  echo types_render_field('gallery-desc') ?></p>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+
+          <?php endif; ?>
+       
+
+
+
+
+
+        <?php if($first) $first = false; ?>
+        <?php endwhile; ?>
+    
+      </div></div>
+
+
+    <?php elseif($num == 5): ?>
+
+        <?php $first = true; while (have_posts()) : the_post();  ?>
+          
+          <?php if($first): ?>
+
+            <div class='col-sm-6'>
+              <div class='gallery-inner'>
+                <a href=' <?php the_permalink(); ?>'>
+                  <?php
+                  if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                    the_post_thumbnail('large', array( 'class' => 'img-responsive' ));
+                  } 
+                  ?>
+                  <div class='text'>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php  echo types_render_field('gallery-desc') ?></p>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class='col-sm-6'>
+              <div class='row'>
+
+          <?php else: ?>
+
+            <div class='col-sm-6'>
               <div class='gallery-inner'>
                 <a href=' <?php the_permalink(); ?>'>
                   <?php
